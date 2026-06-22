f9e1bc10d23150fed9f7c5a4a05a0785f7966c53
Mark Andrey <MarkAndreyDelaCruz@ymail.com>
2026-06-22 09:08:43 +0800

    Add OPUS encoding scripts for 64K, 96K, and 128K bitrates

---
+++ Windows/FFMPEG - OPUS 64K.cmd
@@ -0,0 +1,24 @@
+@echo on
+
+if "%~1"=="" (
+    for %%F in (*.wav *.mp3 *.m4a *.aac *.flac *.ogg *.3gp *.mp4) do (
+        if exist "%%F" call :convert "%%~fF"
+    )
+) else (
+    for %%F in (%*) do if exist "%%~fF" call :convert "%%~fF"
+)
+
+pause
+exit /b
+
+:convert
+ffmpeg -n -i "%~1" ^
+    -c:a libopus ^
+    -b:a 64k ^
+    -vbr on ^
+    -compression_level 10 ^
+    -ar 48000 ^
+    -ac 2 ^
+    "%~dpn1 - OPUS 64K.opus"
+
+exit /b
