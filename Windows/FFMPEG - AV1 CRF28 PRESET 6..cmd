92af6e60dd64d1baf5cc9a831e8d38ab0a91f87a
Mark Andrey <MarkAndreyDelaCruz@ymail.com>
2026-06-22 15:11:09 +0800

    Add AV1 encoding script for CRF28 preset 6

---
+++ Windows/FFMPEG - AV1 CRF28 PRESET 6..cmd
@@ -0,0 +1,56 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set /a count=0
+set /a total=0
+
+if "%~1"=="" (
+    for %%F in (*.mp4 *.mov *.mkv *.avi *.m4v *.webm) do if exist "%%F" set /a total+=1
+    for %%F in (*.mp4 *.mov *.mkv *.avi *.m4v *.webm) do if exist "%%F" (
+        set /a count+=1
+        title [!count!/!total!] Converting: %%~nxF
+        call :convert "%%~fF"
+    )
+) else (
+    for %%A in (%*) do (
+        if exist "%%~fA\" (
+            for %%F in ("%%~fA\*.mp4" "%%~fA\*.mov" "%%~fA\*.mkv" "%%~fA\*.avi" "%%~fA\*.m4v" "%%~fA\*.webm") do set /a total+=1
+        ) else if exist "%%~fA" (
+            set /a total+=1
+        )
+    )
+    for %%A in (%*) do (
+        if exist "%%~fA\" (
+            for %%F in ("%%~fA\*.mp4" "%%~fA\*.mov" "%%~fA\*.mkv" "%%~fA\*.avi" "%%~fA\*.m4v" "%%~fA\*.webm") do (
+                set /a count+=1
+                title [!count!/!total!] Converting: %%~nxF
+                call :convert "%%~fF"
+            )
+        ) else if exist "%%~fA" (
+            set /a count+=1
+            title [!count!/!total!] Converting: %%~nxA
+            call :convert "%%~fA"
+        )
+    )
+)
+
+title Done - !count! file^(s^) converted to AV1
+echo.
+echo Finished: !count! file(s) converted.
+pause
+exit /b
+
+:convert
+ffmpeg -n -i "%~1" ^
+    -c:v libsvtav1 ^
+    -preset 6 ^
+    -crf 28 ^
+    -c:a copy ^
+    "%~dpn1 [AV1 CRF28 P6].mkv"
+
+if !errorlevel! equ 0 (
+    title [!count!/!total!] Done: %~nx1
+) else (
+    title [!count!/!total!] FAILED: %~nx1
+)
+exit /b
